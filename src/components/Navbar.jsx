430dd1932dff422bf3293aecdf44430fe2d87459
Abhinav Maurya <navrya1212@gmail.com>
2026-09-03 22:54:23 +0530

    deploy: Vr.6.9.4

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -92,7 +92,7 @@ export default function Navbar() {
   return (
     <nav className="navbar">
       <div className="nav-brand">
-        <i className="fa-brands fa-java"></i> JavaNest <span className="nav-version">Vr.6.9.3</span>
+        <i className="fa-brands fa-java"></i> JavaNest <span className="nav-version">Vr.6.9.4</span>
       </div>
       <button className="mobile-menu-btn" onClick={e => { e.stopPropagation(); setMenuOpen(!menuOpen); setDropdownOpen(false) }}>
         <i className="fas fa-bars"></i>
