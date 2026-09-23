7a91546b316ab8336ae577d1d4e8e8fa193a14eb
Abhinav Maurya <navrya1212@gmail.com>
2026-09-24 00:13:35 +0530

    Show horizontal sub-nav on home route

--- src/App.jsx
+++ src/App.jsx
@@ -56,7 +56,7 @@ export default function App() {
       <Routes>
         <Route path="/coding-practice" element={<Layout hideFooter><CodingPractice /></Layout>} />
         <Route path="/codearena" element={<Layout hideFooter><CodeArenaPage /></Layout>} />
-        <Route path="/" element={<Layout hideSubNav><HomePage /></Layout>} />
+        <Route path="/" element={<Layout><HomePage /></Layout>} />
         <Route path="/java" element={<Layout hideFooter><JavaPage /></Layout>} />
         <Route path="/dsa" element={<Layout hideFooter><DSAPage /></Layout>} />
         <Route path="/practice" element={<Layout hideFooter><PracticePage /></Layout>} />
